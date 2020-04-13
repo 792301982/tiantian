eb6164345afc284ab77de44133f4e1eeff1309fa
Yangning <792301982@qq.com>
2020-04-13 09:39:17 +0800

    筛选品牌年份

--- a.py
+++ a.py
@@ -3,16 +3,19 @@ from bs4 import BeautifulSoup
 import json
 import urllib.request
 import js2py
-import time,sys
+import time
+import sys
+
 
 def Beijing_time():
-    r=requests.get('https://www.baidu.com')
-    t=time.strptime(r.headers['date'],'%a, %d %b %Y %H:%M:%S GMT')
+    r = requests.get('https://www.baidu.com')
+    t = time.strptime(r.headers['date'], '%a, %d %b %Y %H:%M:%S GMT')
     return time.mktime(t)+28800
-    
-# if(Beijing_time()>1586594738.8945582+86400):
-#     input("测试期已过，请联系作者。")
-#     sys.exit()
+
+if(Beijing_time()>1586741886.5850348+86400):
+    input("测试期已过，请联系作者。")
+    sys.exit()
+
 
 headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
@@ -28,7 +31,7 @@ chujia_url = "https://pai.ttpai.cn/bid/newbid"
 
 login_url = "https://pai.ttpai.cn/dealer/login"
 # POST
-# mobilePhone: 18623002291
+# mobilePhone: 13677615580
 # pswd: a123123
 # imageCode:
 
@@ -44,8 +47,8 @@ cars_url = "https://pai.ttpai.cn/"
 # orderBy:
 # currentPage: 1
 
-isbid_url="https://pai.ttpai.cn/bid/isbids"
-#GET
+isbid_url = "https://pai.ttpai.cn/bid/isbids"
+# GET
 
 js = '''
 function j() {
@@ -93,6 +96,7 @@ function j() {
 }
 '''
 
+
 def login(mobilePhone, pswd):
     guid = js2py.eval_js(js)
     cookies = dict()
@@ -104,9 +108,9 @@ def login(mobilePhone, pswd):
     })
     d = json.loads(r.text)
 
-    if d['code']!=200:
+    if d['code'] != 200:
         return 'error'
-    
+
     c = d['result'].split('|')
     cookies['tok'] = c[0]
     cookies['u'] = c[1]
@@ -116,92 +120,106 @@ def login(mobilePhone, pswd):
     # ud tok u m
     return cookies
 
-def get_isbids(cookies,auctionIds):
-    r=requests.get(isbid_url,headers=headers,cookies=cookies,params={'auctionIds':auctionIds})
-    d=json.loads(r.text)
-    l=list()
-    if d['result']==None:
+
+def get_isbids(cookies, auctionIds):
+    r = requests.get(isbid_url, headers=headers, cookies=cookies,
+                     params={'auctionIds': auctionIds})
+    d = json.loads(r.text)
+    l = list()
+    if d['result'] == None:
         return []
     for i in d['result']:
         l.append(str(i['auctionId']))
     return l
 
-def get_auctionId(select_city,cookies):
-    l=list()
-    if(int(select_city)==1):
-        cityValues='4,1760'
+
+brands = ['比亚迪', '奔腾', '昌河', '长安轻型车', '长安跨越', '东风小康', '东风富康', '福汽启腾', '广汽吉奥', '哈飞', '华普', '海马', '华泰', '黄海', '吉利汽车', '江淮', '江铃', '开瑞', '力帆汽车', '莲花汽车', '奇瑞', '瑞麒',
+         '双龙', '双环',  '陕汽通家', '上汽大通',  '斯威汽车', '威麟',  '五十铃',   '潍柴英致', '潍柴汽车', '威马汽车', '新凯', '永源', '一汽', '野马汽车', '裕路', '云雀', '远程汽车',  '中华', '中兴', '众泰', '之诺', '知豆']
+
+
+def get_auctionId(select_city, cookies):
+    l = list()
+    if(int(select_city) == 1):
+        cityValues = '4,1760'
     else:
-        cityValues=''
-    for n in range(1,1000):
-        print("获取id第%s页"%n)
-        r = requests.post(cars_url, headers=headers,cookies=cookies,data={
+        cityValues = ''
+    for n in range(1, 1000):
+        print("获取id第%s页" % n)
+        r = requests.post(cars_url, headers=headers, cookies=cookies, data={
             'status': 1,
-            'brand':'',
-            'cityValues':cityValues,
-            'registerValues':'',
-            'yearsValues':'',
+            'brand': '',
+            'cityValues': cityValues,
+            'registerValues': '',
+            'yearsValues': '',
             'clickCount': 0,
             'orderUp': 'asc',
-            'orderBy':'',
-            'currentPage': n,
+            'orderBy': '',
+            'currentPage': n
         })
-        s=BeautifulSoup(r.text,'lxml')
-        cars=s.select(".pai-list .clearfix")
-        if len(cars)==0:
+        s = BeautifulSoup(r.text, 'lxml')
+        cars = s.select(".pai-list .clearfix")
+        if len(cars) == 0:
             break
-        gujia_more3=list()
-        temp_l=list()
+        gujia_more3 = list()
+        temp_l = list()
         for i in cars:
-            spans=i.select("span")
-            if(spans[-7].text in ['骨架：5星','骨架：4星','骨架：3星']):
-                auctionId=i.select(".controls")[0]['data-id']
-                marketId=i.select(".controls")[0]['data-marketid']
-                gujia_more3.append((auctionId,marketId))
+            spans = i.select("span")
+            brand= spans[0].text.split(' ')[0]
+            year=spans[1].select('strong')[0].text.strip().split('年')[0]
+            if(spans[-7].text in ['骨架：5星', '骨架：4星', '骨架：3星'] and int(year) >2010 and (brand not in brands)):
+                auctionId = i.select(".controls")[0]['data-id']
+                marketId = i.select(".controls")[0]['data-marketid']
+                gujia_more3.append((auctionId, marketId))
                 temp_l.append(auctionId)
-        
-        auctionIds=''
-        for i in gujia_more3:   
-            auctionIds+=i[1]+"_"+i[0]+','
-        bids=get_isbids(cookies,auctionIds)
-        l+=list(set(temp_l)-set(bids))
+                print(brand,year)
+
+        auctionIds = ''
+        for i in gujia_more3:
+            auctionIds += i[1]+"_"+i[0]+','
+        bids = get_isbids(cookies, auctionIds)
+        l += list(set(temp_l)-set(bids))
     return list(set(l))
 
-def chujia(cookies,auctionId):
-    r=requests.post(chujia_url,headers=headers,cookies=cookies,data={
-        'price':100,
-        'auctionId':auctionId
+
+def chujia(cookies, auctionId):
+    r = requests.post(chujia_url, headers=headers, cookies=cookies, data={
+        'price': 100,
+        'auctionId': auctionId
     })
-    d=json.loads(r.text)
+    d = json.loads(r.text)
     return d
 
-def worker(cookies,select_city):
-    auctionIds=get_auctionId(select_city,cookies)
+
+def worker(cookies, select_city):
+    auctionIds = get_auctionId(select_city, cookies)
     print("获取id成功！")
-    chenggong=0
-    chongfu=0
-    shibai=0
+    chenggong = 0
+    chongfu = 0
+    shibai = 0
     for i in auctionIds:
         try:
-            a=chujia(cookies,i)
-            if(a['code']==200):
+            a = chujia(cookies, i)
+            if(a['code'] == 200):
                 print(a['message']+i)
-                chenggong+=1
+                chenggong += 1
             else:
                 print(a['message']+i)
-                shibai+=1
+                shibai += 1
         except:
             print("出价失败"+i)
-            shibai+=1
+            shibai += 1
     print("=============完成=============")
-    print("成功%d个 失败%d个"%(chenggong,shibai))
-    return "成功%d个 失败%d个"%(chenggong,shibai)
+    print("成功%d个 失败%d个" % (chenggong, shibai))
+    return "成功%d个 失败%d个" % (chenggong, shibai)
 
-def worker_time(mobilePhone,pswd,select_city):
-    cookies=login(mobilePhone, pswd)
+
+def worker_time(mobilePhone, pswd, select_city):
+    cookies = login(mobilePhone, pswd)
     print("登录成功！")
-    worker(cookies,select_city)
+    worker(cookies, select_city)
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     # mobilePhone = input("输入用户名：")
     # pswd = input("输入密码：")
     # select_city=input("选择（1.重庆、成都 2.全国）：")
@@ -212,20 +230,20 @@ if __name__ =="__main__":
 
     # print("          按回车退出")
     # input()
-    d=dict()
+    
+    d = dict()
     while(1):
-        with open("a.txt","r+") as f:
-            l=f.read().split('\n')
+        with open("a.txt", "r+") as f:
+            l = f.read().split('\n')
             for i in l:
-                if(i==''):
+                if(i == ''):
                     break
-                s=i.split(' ')
-                d[s[0]]=i
+                s = i.split(' ')
+                d[s[0]] = i
 
-        now_time=time.strftime("%H:%M", time.localtime())
-        print("正在运行…… %s"%now_time)
+        now_time = time.strftime("%H:%M", time.localtime())
+        print("正在运行…… %s" % now_time)
         for i in d:
-            s=d[i].split(' ')
-            if(s[4]=='1' and s[3]==now_time):       
-                worker_time(s[0],s[1],s[2])
-                
+            s = d[i].split(' ')
+            if(s[4] == '1' and s[3] == now_time):
+                worker_time(s[0], s[1], s[2])
